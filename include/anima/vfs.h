cdeddd428366acef3ca127368b3492ec74e0b42e
Maxime Poncet <maxime.poncet@gmail.com>
2015-05-28 17:06:56 +0200

    Add unstaged changes.

--- include/anima/vfs.h
+++ include/anima/vfs.h
@@ -1,6 +1,8 @@
 #ifndef __VFS_H
 #define __VFS_H
 
+#include <linux/kernel.h>
+
 typedef int (*__filldir_t)(void *, const char *, int, loff_t, u64, unsigned);
 struct __dir_context {
 	__filldir_t actor;
